48f5f269cfd2a8ee75909a32a7471313cdb905eb
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-21 18:11:39 -0800

    Add env gem and feature
    configured env in application.rb, created a new .env file, added env[api_key] to pictures show controller

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,24 @@ module NasaPicApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+
     config.api_only = true
 
+    # Cors give persmission to localhost 300
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
+
+    # env file 
+    Bundler.require(*Rails.groups)
+
+    Dotenv::Railtie.load
+
+    HOSTNAME = ENV['HOSTNAME']
 
   end
 end
