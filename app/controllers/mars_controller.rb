22837ce03d87013625b1cdf34a8462472893fc9b
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-24 15:42:19 -0800

    Add mars index controller
    controller will retrieve 25 photos from Mars Rover

--- app/controllers/mars_controller.rb
+++ app/controllers/mars_controller.rb
@@ -1,9 +1,14 @@
 class MarsController < ApplicationController
 
-    def show
+    def index
       @mars = RestClient::Request.execute(:method => "get",
-        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&camera=all&api_key="+ENV['API_KEY']
+        :url => "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&sol=1000&api_key="+ENV['API_KEY'])
         render json: @mars
     end
 
 end
+
+#
+# https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=${camera}&api_key=DEMO_KEY
+#
+# https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&sol=1000&api_key=
