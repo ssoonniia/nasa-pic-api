2fa6f9841d90eea2d8de94f4b8e16d9aebccf1ff
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-26 15:36:44 -0800

    Update routes params to date vs id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pictures
+  get'pictures/:date', to: 'pictures#show'
   resources :mars
 
   root 'pictures#show'
