898f56c4c4cb844f48f55f435271cb43b675af11
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 13:15:08 -0800

    Add comments create controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
+  # before_action :authenticate_user
 
-  
+  def index
+  end
+
+  def create
+    @comment = Comment.create(content: params[:content])
+    @picture = Picture.find_by(date: params[:date])
+    @comment.picture = @picture
+
+    if @comment.save
+      render :json => @comment
+    else
+      render json: {message: @comment.errors}, status:400
+    end
+
+  end
+
+
 
 end
