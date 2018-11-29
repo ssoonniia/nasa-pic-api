02fbcf047c2374e0b476e8e1b36a91ce6498a24b
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 20:49:00 -0800

    fix spacing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,9 @@
 class CommentsController < ApplicationController
-  # before_action :authenticate_user
 
   def index
   end
 
   def create
-    debugger
     @comment = Comment.create(content: params[:content], author: params[:author])
     @picture = Picture.find_by(date: params[:date])
     @comment.picture = @picture
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,20 @@
-class PicturesController < ApplicationController
+  class PicturesController < ApplicationController
 
-  def index
-    @pictures = Picture.all
-    render json: pictures
+    def index
+       @pictures = Picture.all
+       render json: pictures
 
-  end
-
+     end
 
   def show
-
-    @date = !params[:date] ? Date.today : params[:date]
-    @picture = Picture.find_or_create_by(date: @date)
-
+    date = !params[:date] ? Date.today : params[:date]
+    @picture = Picture.find_or_create_by(date: date)
     if !@picture.title
       @response = RestClient::Request.execute(:method => "get",
         :url => "https://api.nasa.gov/planetary/apod?date=#{date}&hd=false&api_key="+ENV['API_KEY'])
-      @pictureObj =  JSON.parse(response)
-
+      @pictureObj =  JSON.parse(@response)
       @picture.update(@pictureObj)
     end
-
     render json: @picture, include: ['comments']
   end
 
