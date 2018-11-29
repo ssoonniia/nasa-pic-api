353b6e7f50ee1a044c94b8d0ba62e310cad6cc39
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 20:22:27 -0800

    Drop table, removed user since never user

--- db/migrate/20181127230246_create_comments.rb
+++ db/migrate/20181127230246_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.text :content
+      t.text :author
+      t.text :picture_id
     end
   end
 end
