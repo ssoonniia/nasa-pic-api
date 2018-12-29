1278af416e080b7e8845048fb072caac4294f8d9
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 12:09:32 -0500

    Add helper functions to Picture Controller
    Add set_date, set_picture and get_picture_from_nasa. Removed logic from show controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,20 +1,34 @@
-  class PicturesController < ApplicationController
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show]
+  before_action :set_date, only: [:show]
 
-    def index
-      @pictures = Picture.all
-      render json: @pictures
-     end
+  def index
+    @pictures = Picture.all
+    render json: @pictures
+  end
 
   def show
-    date = !params[:date] ? Date.today : params[:date]
-    @picture = Picture.find_or_create_by(date: date)
     if !@picture.title
-      response = RestClient::Request.execute(:method => "get",
-        :url => "https://api.nasa.gov/planetary/apod?date=#{date}&hd=false&api_key="+ENV['API_KEY'])
-      @pictureObj =  JSON.parse(response)
-      @picture.update(@pictureObj)
+      get_picture_from_nasa
     end
     render json: @picture, include: ['comments']
+  end
+
+ private
+
+  def set_date
+    @date = !params[:date] ? Date.today : params[:date]
+  end
+
+  def set_picture
+    @picture = Picture.find_or_create_by(date: set_date)
+  end
+
+  def get_picture_from_nasa
+    response = RestClient::Request.execute(:method => "get",
+      :url => "https://api.nasa.gov/planetary/apod?date=#{@date}&hd=false&api_key="+ENV['API_KEY'])
+    pictureObj =  JSON.parse(response)
+    @picture.update(pictureObj)
   end
 
 end
