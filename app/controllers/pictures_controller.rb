8b44de54333524e04e58ad37552f158a5da066b6
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 11:17:14 -0500

    Change response from instance variable to local variable

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,9 +9,9 @@
     date = !params[:date] ? Date.today : params[:date]
     @picture = Picture.find_or_create_by(date: date)
     if !@picture.title
-      @response = RestClient::Request.execute(:method => "get",
+      response = RestClient::Request.execute(:method => "get",
         :url => "https://api.nasa.gov/planetary/apod?date=#{date}&hd=false&api_key="+ENV['API_KEY'])
-      @pictureObj =  JSON.parse(@response)
+      @pictureObj =  JSON.parse(response)
       @picture.update(@pictureObj)
     end
     render json: @picture, include: ['comments']
